73057935077228e90da03fd140e8fae8f44be00a
Tim Jenness <t.jenness@jach.hawaii.edu>
2007-05-21 23:36:08 +0000

    svn and cvs versions differ so code to extract Revision string needs to change
    svn path=/trunk/perlmods/; revision=11312

--- Jiggle.pm
+++ Jiggle.pm
@@ -31,7 +31,7 @@ use Astro::Coords::Offset;
 
 use vars qw/ $VERSION /;
 
-$VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d", q$Revision$ =~ /(\d+)/);
 
 =head1 METHODS
 
